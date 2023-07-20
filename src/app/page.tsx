a1670e15ec565e19b33517fb336ca088725639db
susumu tomita <oyster880@gmail.com>
2023-07-21 07:38:30 +0900

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from 'next/image'
-
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p >
-          WELCOME Get started by editing here&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+        <h1>hoge</h1>
+        <p>fuga</p>
       </div>
     </main>
   )
