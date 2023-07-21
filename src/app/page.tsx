3189744713aea7063b49caf115581e1f16670fa5
susumu tomita <oyster880@gmail.com>
2023-07-22 06:38:02 +0900

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-export default function Page() {
-  return (
+const Page = () => (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <h1>hoge</h1>
         <p>fuga</p>
       </div>
     </main>
-  )
-}
+)
+
+export default Page;
