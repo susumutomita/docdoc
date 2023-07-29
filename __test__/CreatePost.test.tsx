43e1c75d6ff05511d1226acaf0668fb8fec0d45a
susumu tomita <oyster880@gmail.com>
2023-07-30 06:33:08 +0900

    add test

---
+++ __test__/CreatePost.test.tsx
@@ -0,0 +1,9 @@
+/**
+* @jest-environment jsdom　　　　
+*/
+import '@testing-library/jest-dom/extend-expect'
+describe('User can create Post', () => {
+  it('create 1 post', () => {
+    expect(1).toBe(1)
+  })
+})
