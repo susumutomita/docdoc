23dd8c170dc655da4b74b77ae5d8ba15bb3dc9f4
susumu tomita <oyster880@gmail.com>
2023-08-02 08:17:55 +0900

    update test

--- __test__/app/services/postsService.test.ts
+++ __test__/app/services/postsService.test.ts
@@ -10,43 +10,15 @@ import fetchMock from 'jest-fetch-mock';
 fetchMock.enableMocks();
 
 describe('createPost', () => {
-  beforeEach(() => {
-    fetchMock.resetMocks();
-  });
-
   it('creates a new post when title and content are given', async () => {
-    // モックのレスポンスをセットする
-    fetchMock.mockResponseOnce(
-      JSON.stringify({
-        id: '123',
-        title: 'My First Post',
-        content: 'This is my first post.',
-        createdAt: '2023-07-23T12:34:56Z',
-        updatedAt: '2023-07-23T12:34:56Z',
-      }),
-    );
-
     const post = await createPost('My First Post', 'This is my first post.');
 
-    // fetchが正しいURLとオプションで呼び出されたことを確認する
-    console.log(fetchMock.mock.calls[0][0]);
-    expect(fetchMock.mock.calls[0][0]).toEqual('/api/posts');
-    expect(fetchMock.mock.calls[0][1]).toEqual({
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        title: 'My First Post',
-        content: 'This is my first post.',
-      }),
-    });
-
-    // 返されたデータが正しいことを確認する
     expect(post).toEqual({
       id: '123',
       title: 'My First Post',
       content: 'This is my first post.',
-      createdAt: new Date('2023-07-23T12:34:56Z'),
-      updatedAt: new Date('2023-07-23T12:34:56Z'),
+      createdAt: expect.any(Date),
+      updatedAt: expect.any(Date),
     });
   });
 
