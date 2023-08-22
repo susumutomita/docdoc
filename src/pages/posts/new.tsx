89ebe3f0acba70ae6f3ffc652d95aad066fc496a
susumu tomita <oyster880@gmail.com>
2023-08-23 07:01:27 +0900

    add pages

--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -3,7 +3,7 @@ import PostForm from '../../components/PostForm';
 export default function NewPostPage() {
   return (
     <div>
-      <h1>新しい記事を投稿する</h1>
+      <h1>新しい記事を投稿する.</h1>
       <PostForm />
     </div>
   );
