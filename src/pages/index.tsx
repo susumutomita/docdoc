44dcc45bf893dfbdd675de3544dead904346a37a
susumu tomita <oyster880@gmail.com>
2023-08-24 05:46:54 +0900

    add pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import PostItem from '../components/PostItem';
+import { Post } from '../app/posts/PostsType';
 
 const PostList = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState<Post[]>([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -17,9 +18,11 @@ const PostList = () => {
   return (
     <div>
       <h1>投稿一覧</h1>
-      {posts.map(post => (
-        <PostItem key={post.id} post={post} />
-      ))}
+      {posts.length > 0 ? (
+        posts.map(post => <PostItem key={post.id} post={post} />)
+      ) : (
+        <p>投稿がありません。</p>
+      )}
     </div>
   );
 };
