b320799059e3025ffdcda502f267f7d74cbd14e5
susumu tomita <oyster880@gmail.com>
2024-05-15 07:00:40 +0900

    update user create and prisma studio

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,6 +3,30 @@ import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
+export async function POST(req: Request) {
+  try {
+    const { name, email, posts, profile } = await req.json();
+    const user = await prisma.user.create({
+      data: {
+        name: name,
+        email: email,
+        posts: {
+          create: { title: posts },
+        },
+        profile: {
+          create: { bio: profile },
+        },
+      },
+    });
+    return NextResponse.json(user);
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'An error occurred while creating the user.' },
+      { status: 500 },
+    );
+  }
+}
+
 export async function GET() {
   try {
     const users = await prisma.user.findMany();
